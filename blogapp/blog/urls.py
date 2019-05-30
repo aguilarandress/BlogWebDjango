8f7f583353b0be2820ff90c8cefa24497ca05d0c
AndrewGutierrezCastro <andrewgc@estudiantec.cr>
2019-05-29 18:51:18 -0600

    se agrega el view de paginas incio y crear
    se agrega el view de paginas incio y crear usuario, junto con cajas de input para introducir la informacion

--- blogapp/blog/urls.py
+++ blogapp/blog/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("inicioSesion/crearUsuario/", views.crearUsuario,  name="crearUsuario"),
+    path("inicioSesion/", views.inicioSesion,  name="inicioSesion"),
+
+
 ]
--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -16,3 +16,36 @@ def index(request):
         "titulo": "TEC Blog, by Andrew & Andres"
     }
     return render(request, "blog/index.html", context)
+# Create your views here.
+
+
+def inicioSesion(request):
+    """Carga la página iniciar sesion en el blog
+
+        Entradas:
+            request: Es un objeto que representa un request
+        Precondiciones:
+            No hay
+        Salidas:
+            Retorna una respuesta al usuario
+        """
+    context = {
+        "titulo": "TEC Blog, inicio de sesión"
+    }
+    return render(request, "blog/inicioSesion.html", context)
+
+
+def crearUsuario(request):
+    """Carga la página crearUsuarios del blog
+
+        Entradas:
+            request: Es un objeto que representa un request
+        Precondiciones:
+            No hay
+        Salidas:
+            Retorna una respuesta al usuario
+        """
+    context = {
+        "titulo": "TEC Blog, crear usuario"
+    }
+    return render(request, "blog/crearUsuario.html", context)
