c3b730f63d1b9912702bae1d298ea608ab20c5c8
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2019-06-04 14:34:49 -0600

    Control de acceso

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .helpers import validarDatosDeRegistro, validarDatosInicioSesion
 
@@ -40,39 +41,42 @@ def registrarNuevoUsuario(request):
         retorna un HttpResponseRedirect cuando un usuario se
         registra con éxito
     """
-    if request.method == "POST":
-        # Obtener datos del formulario
-        usuario = {
-            "nombre": request.POST["nombre"],
-            "email": request.POST["email"],
-            "contraseña": request.POST["contraseña"],
-            "contraseña2": request.POST["contraseña2"]
-        }
-
-        # Validar datos del formulario
-        if not validarDatosDeRegistro(usuario)["esValido"]:
-            for mensaje in validarDatosDeRegistro(usuario)["mensajes"]:
-                messages.error(request, mensaje)
-            return render(request, "blog/registrar.html", usuario)
-
-        # Revisar si existe un usuario con el nombre
-        if User.objects.filter(username=usuario["nombre"]).exists():
-            messages.error(request, "El nombre de usuario ya existe")
-            return render(request, "blog/registrar.html", usuario)
-
-        # Revisar si existe un usuario con el nombre
-        if User.objects.filter(email=usuario["email"]).exists():
-            messages.error(request, "El correo utilizado ya existe")
-            return render(request, "blog/registrar.html", usuario)
-
-        # Registrar usuario
-        usuarioNuevo = User.objects.create_user(
-            usuario["nombre"], usuario["email"], usuario["contraseña"])
-        usuarioNuevo.save()
-        messages.success(request, "Usuario registrado")
-        return HttpResponseRedirect(reverse("blog:iniciarSesion"))
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("blog:index"))
     else:
-        return render(request, "blog/registrar.html")
+        if request.method == "POST":
+            # Obtener datos del formulario
+            usuario = {
+                "nombre": request.POST["nombre"],
+                "email": request.POST["email"],
+                "contraseña": request.POST["contraseña"],
+                "contraseña2": request.POST["contraseña2"]
+            }
+
+            # Validar datos del formulario
+            if not validarDatosDeRegistro(usuario)["esValido"]:
+                for mensaje in validarDatosDeRegistro(usuario)["mensajes"]:
+                    messages.error(request, mensaje)
+                return render(request, "blog/registrar.html", usuario)
+
+            # Revisar si existe un usuario con el nombre
+            if User.objects.filter(username=usuario["nombre"]).exists():
+                messages.error(request, "El nombre de usuario ya existe")
+                return render(request, "blog/registrar.html", usuario)
+
+            # Revisar si existe un usuario con el nombre
+            if User.objects.filter(email=usuario["email"]).exists():
+                messages.error(request, "El correo utilizado ya existe")
+                return render(request, "blog/registrar.html", usuario)
+
+            # Registrar usuario
+            usuarioNuevo = User.objects.create_user(
+                usuario["nombre"], usuario["email"], usuario["contraseña"])
+            usuarioNuevo.save()
+            messages.success(request, "Usuario registrado")
+            return HttpResponseRedirect(reverse("blog:iniciarSesion"))
+        else:
+            return render(request, "blog/registrar.html")
 
 
 def iniciarSesion(request):
@@ -87,30 +91,40 @@ def iniciarSesion(request):
         retorna un HttpResponseRedirect cuando un usuario se
         registra con éxito
     """
-    if request.method == "POST":
-        # Obtener datos del formulario
-        usuario = {"nombre": request.POST["nombre"],
-                   "contraseña": request.POST["contraseña"]}
-        # Validar datos del formulario
-        if not validarDatosInicioSesion(usuario)["esValido"]:
-            for mensaje in validarDatosInicioSesion(usuario)["mensajes"]:
-                messages.error(request, mensaje)
-            return render(request, "blog/iniciarSesion.html", usuario)
-        # Autorizar al usuario
-        usuario = authenticate(
-            request, username=usuario["nombre"], password=usuario["contraseña"])
-        if not usuario:
-            messages.error(
-                request, "Nombre de usuario o contraseña incorrectos")
-            return render(request, "blog/iniciarSesion.html")
-        else:
-            login(request, usuario)
-            messages.success(request, "Ha iniciado sesión correctamente")
-            return HttpResponseRedirect(reverse("blog:index"))
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("blog:index"))
     else:
-        return render(request, "blog/iniciarSesion.html")
+        if request.method == "POST":
+            # Obtener datos del formulario
+            usuario = {"nombre": request.POST["nombre"],
+                       "contraseña": request.POST["contraseña"]}
+            # Validar datos del formulario
+            if not validarDatosInicioSesion(usuario)["esValido"]:
+                for mensaje in validarDatosInicioSesion(usuario)["mensajes"]:
+                    messages.error(request, mensaje)
+                return render(request, "blog/iniciarSesion.html", usuario)
+            # Autorizar al usuario
+            usuario = authenticate(
+                request, username=usuario["nombre"], password=usuario["contraseña"])
+            if not usuario:
+                messages.error(
+                    request, "Nombre de usuario o contraseña incorrectos")
+                return render(request, "blog/iniciarSesion.html")
+            else:
+                login(request, usuario)
+                messages.success(request, "Ha iniciado sesión correctamente")
+                return HttpResponseRedirect(reverse("blog:index"))
+        else:
+            return render(request, "blog/iniciarSesion.html")
 
 
+def cerrarSesion(request):
+    logout(request)
+    messages.success(request, "Ha cerrado su sesión correctamente")
+    return HttpResponseRedirect(reverse("blog:iniciarSesion"))
+
+
+@login_required(login_url='/iniciarSesion/')
 def crearPost(request):
     context = {
         "titulo": "Escriba su post"
