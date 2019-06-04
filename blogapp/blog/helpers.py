ce56de87146571b1f467908749e22485850c4aea
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2019-06-03 22:29:57 -0600

    Validación de datos de formulario de registro

--- blogapp/blog/helpers.py
+++ blogapp/blog/helpers.py
@@ -1,6 +1,44 @@
 import re
 
 
+# Validar datos de registro
+def validarDatosDeRegistro(usuario):
+    """Valida los datos del formulario
+
+    Entradas:
+        usuario: Es un diccionario
+    Precondiciones:
+        usuario contiene los datos del formulario de registro
+    Salidas:
+        Retorna un diccionario que contiene si los datos están válidos
+        y los mensajes de error
+    Proceso:
+        1. Se crea una variable resultado
+        2. Se revisa si los están completos
+        3. Se revisa el formato del correo
+        4. Se comparan las contraseñas
+        5. Se retorna el resultado
+    """
+    resultado = {"esValido": True, "mensajes": []}
+    # Revisar que los datos estén completos
+    for dato in usuario:
+        if not usuario[dato]:
+            resultado["esValido"] = False
+            resultado["mensajes"].append("Por favor complete el formulario completamente")
+            return resultado
+
+    # Revisar el formato del correo
+    if not validarCorreos(usuario["email"]):
+        resultado["mensajes"].append("El formato del correo no es correcto")
+    # Comparar contraseñas
+    if not (usuario["contraseña"] == usuario["contraseña2"]):
+        resultado["mensajes"].append("Ambas contraseñas deben ser iguales")
+
+    if len(resultado["mensajes"]) > 0:
+        resultado["esValido"] = False
+    return resultado
+
+
 # Validar formato de correo
 def validarCorreos(stringEntrada):
     """Valida si el string tiene un formato de correo o no
@@ -20,6 +58,3 @@ def validarCorreos(stringEntrada):
     if patron.search(stringEntrada):
         return True
     return False
-
-
-
--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from .helpers import validarDatosDeRegistro
 
 
 def index(request):
@@ -35,11 +36,21 @@ def registrarNuevoUsuario(request):
         registra con éxito
     """
     if request.method == "POST":
-        nombreDeUsuario = request.POST["nombre"]
-        email = request.POST["email"]
-        contraseña = request.POST["contraseña"]
-        usuarioNuevo = User.objects.create_user(nombreDeUsuario, email, contraseña)
+        # Obtener datos del formulario
+        usuario = {
+            "nombre": request.POST["nombre"],
+            "email": request.POST["email"],
+            "contraseña": request.POST["contraseña"],
+            "contraseña2": request.POST["contraseña2"]
+        }
+        # TODO: Validar datos del formulario
+        if not validarDatosDeRegistro(usuario)["esValido"]:
+            for mensaje in validarDatosDeRegistro(usuario)["mensajes"]:
+                messages.error(request, mensaje)
+            return render(request, "blog/registrar.html", usuario)
+        usuarioNuevo = User.objects.create_user(usuario["nombre"], usuario["email"], usuario["contraseña"])
         usuarioNuevo.save()
+
         messages.success(request, "Usuario registrado")
         return HttpResponseRedirect(reverse("blog:iniciarSesion"))
     else:
@@ -70,8 +81,3 @@ def iniciarSesion(request):
             return HttpResponseRedirect(reverse("blog:iniciarSesion"))
     else:
         return render(request, "blog/iniciarSesion.html")
-
-
-MESSAGE_TAGS = {
-    ""
-}
